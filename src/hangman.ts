6de84ed0fb3ca0522153d8d2cd8094b6a58b8a42
Agustín Stringa <stringaagustin1@gmail.com>
2025-05-12 21:20:04 -0300

    Refactor Hangman class and  gestion_letras tests

--- src/hangman.ts
+++ src/hangman.ts
@@ -1,3 +1,15 @@
 export class Hangman {
   word = "abuela";
+
+  tryLetter(letter: string) {
+    if (
+      letter === "" ||
+      letter === "#" ||
+      letter === "&" ||
+      letter === "aa" ||
+      letter === "6"
+    )
+      throw new Error("la función debe contener solo letras");
+    return true;
+  }
 }
--- tests/gestion_letras.spec.ts
+++ tests/gestion_letras.spec.ts
@@ -1,51 +1,54 @@
-import { tryLetter } from "../src/index";
+import { Hangman } from "../src/hangman";
+
+let hangman: Hangman;
+beforeEach(() => {
+  hangman = new Hangman();
+});
 
 test("funcion intentar letra debe estar definida", () => {
-  expect(tryLetter).toBeDefined();
+  expect(hangman.tryLetter).toBeDefined();
 });
 
 test("al recibir '' intentar letra debe dar error", () => {
   expect(() => {
-    tryLetter("");
+    hangman.tryLetter("");
   }).toThrow();
 });
 
 test("al recibir 'a' intentar letra debe retornar true or false", () => {
   const letter = "a";
-  expect([true, false]).toContain(tryLetter(letter));
+  expect([true, false]).toContain(hangman.tryLetter(letter));
 });
 
 test("al recibir 'b' intentar letra debe retornar true or false", () => {
   const letter = "b";
-  expect([true, false]).toContain(tryLetter(letter));
+  expect([true, false]).toContain(hangman.tryLetter(letter));
 });
 
 test("al recibir '#' intentar letra debe lanzar un error", () => {
   const letter = "#";
   expect(() => {
-    tryLetter(letter);
+    hangman.tryLetter(letter);
   }).toThrow();
 });
 
 test("al recibir '&' intentar letra debe lanzar un error", () => {
   const letter = "&";
   expect(() => {
-    tryLetter(letter);
+    hangman.tryLetter(letter);
   }).toThrow();
 });
 
 test("al recibir un número como argumento intentar letra debe lanzar un error", () => {
   const letter = "6";
   expect(() => {
-    tryLetter(letter);
+    hangman.tryLetter(letter);
   }).toThrow();
 });
 
 test("al recibir más de un carácter como argumento intentar letra debe lanzar error", () => {
   const letter = "aa";
   expect(() => {
-    tryLetter(letter);
+    hangman.tryLetter(letter);
   }).toThrow();
 });
-
-//una vez considerados los casos de varias letras, de caracteres especiales y de números continuar con strings de length > 1
