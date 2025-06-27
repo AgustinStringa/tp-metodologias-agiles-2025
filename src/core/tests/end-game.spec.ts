25fe2e513b2578c3385f9440b730f71cca214ede
AaronDeBernardo <142444363+AaronDeBernardo@users.noreply.github.com>
2025-06-27 18:48:53 -0300

    refactor: grouping tests in end-game.spec.ts
    Co-Authored-By: Elias Tomas Danteo <163892255+EliasDanteo@users.noreply.github.com>
    Co-Authored-By: Joaquin Carlos Fernandez Da Silva <164550681+Joaquipe13@users.noreply.github.com>
    Co-Authored-By: Agustín Stringa <85123081+AgustinStringa@users.noreply.github.com>

--- src/core/tests/end-game.spec.ts
+++ src/core/tests/end-game.spec.ts
@@ -6,55 +6,57 @@ beforeEach(() => {
   hangman = new Hangman();
 });
 
-test('Si la palabra es "auto", intento A, U, T, O, el estado del juego debe ser "WON"', () => {
-  hangman.word = "auto";
+describe("Transiciones del estado del juego", () => {
+  test('Si la palabra es "auto", intento A, U, T, O, el estado del juego debe ser "WON"', () => {
+    hangman.word = "auto";
 
-  hangman.tryLetter("A");
-  hangman.tryLetter("U");
-  hangman.tryLetter("T");
-  hangman.tryLetter("O");
+    hangman.tryLetter("A");
+    hangman.tryLetter("U");
+    hangman.tryLetter("T");
+    hangman.tryLetter("O");
 
-  const gameStatus = hangman.getGameStatus();
-  const expectedGameStatus = "WON";
-  expect(gameStatus).toEqual(expectedGameStatus);
-});
+    const gameStatus = hangman.getGameStatus();
+    const expectedGameStatus = "WON";
+    expect(gameStatus).toEqual(expectedGameStatus);
+  });
 
-test('Si la palabra es "auto", intento J, K, L, M, N, Z, H el estado del juego debe ser "LOST"', () => {
-  hangman.word = "auto";
-  hangman.tryLetter("J");
-  hangman.tryLetter("K");
-  hangman.tryLetter("L");
-  hangman.tryLetter("M");
-  hangman.tryLetter("N");
-  hangman.tryLetter("Z");
-  hangman.tryLetter("H");
+  test('Si la palabra es "auto", intento J, K, L, M, N, Z, H el estado del juego debe ser "LOST"', () => {
+    hangman.word = "auto";
+    hangman.tryLetter("J");
+    hangman.tryLetter("K");
+    hangman.tryLetter("L");
+    hangman.tryLetter("M");
+    hangman.tryLetter("N");
+    hangman.tryLetter("Z");
+    hangman.tryLetter("H");
 
-  const gameStatus = hangman.getGameStatus();
-  const expectedGameStatus = "LOST";
-  expect(gameStatus).toEqual(expectedGameStatus);
-});
+    const gameStatus = hangman.getGameStatus();
+    const expectedGameStatus = "LOST";
+    expect(gameStatus).toEqual(expectedGameStatus);
+  });
 
-test('Si la palabra es "auto", intento T, A el estado del juego debe ser "IN PROGRESS"', () => {
-  hangman.word = "auto";
-  hangman.tryLetter("T");
-  hangman.tryLetter("A");
+  test('Si la palabra es "auto", intento T, A el estado del juego debe ser "IN PROGRESS"', () => {
+    hangman.word = "auto";
+    hangman.tryLetter("T");
+    hangman.tryLetter("A");
 
-  const gameStatus = hangman.getGameStatus();
-  const expectedGameStatus = "IN PROGRESS";
-  expect(gameStatus).toEqual(expectedGameStatus);
-});
+    const gameStatus = hangman.getGameStatus();
+    const expectedGameStatus = "IN PROGRESS";
+    expect(gameStatus).toEqual(expectedGameStatus);
+  });
 
-test('Si la palabra es "carro", intento C, A, R, O, el estado del juego debe ser "WON"', () => {
-  hangman.word = "carro";
+  test('Si la palabra es "carro", intento C, A, R, O, el estado del juego debe ser "WON"', () => {
+    hangman.word = "carro";
 
-  hangman.tryLetter("C");
-  hangman.tryLetter("A");
-  hangman.tryLetter("R");
-  hangman.tryLetter("O");
+    hangman.tryLetter("C");
+    hangman.tryLetter("A");
+    hangman.tryLetter("R");
+    hangman.tryLetter("O");
 
-  const gameStatus = hangman.getGameStatus();
-  const expectedGameStatus = "WON";
-  expect(gameStatus).toEqual(expectedGameStatus);
+    const gameStatus = hangman.getGameStatus();
+    const expectedGameStatus = "WON";
+    expect(gameStatus).toEqual(expectedGameStatus);
+  });
 });
 
 test('Si la palabra es "pez", intento P, E, Z y luego A, tryLetter() debería lanzar un error.', () => {
